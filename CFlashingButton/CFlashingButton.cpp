8dd222e9314cb03b0ce576bfbe2449e7c8b09725
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-05-07 18:03:26 -0500

    Implement button flashing

--- CFlashingButton/CFlashingButton.cpp
+++ CFlashingButton/CFlashingButton.cpp
@@ -11,7 +11,9 @@ IMPLEMENT_DYNAMIC(CFlashingButton, CButton)
 
 CFlashingButton::CFlashingButton()
 {
-
+    m_shouldIFlash = false;
+    m_timerID = 0;
+    m_elapse = 500;
 }
 
 CFlashingButton::~CFlashingButton()
@@ -20,6 +22,7 @@ CFlashingButton::~CFlashingButton()
 
 
 BEGIN_MESSAGE_MAP(CFlashingButton, CButton)
+    ON_WM_TIMER()
 END_MESSAGE_MAP()
 
 
@@ -27,3 +30,93 @@ END_MESSAGE_MAP()
 // CFlashingButton message handlers
 
 
+
+
+void CFlashingButton::OnTimer(UINT_PTR nIDEvent)
+{
+    if (m_timerID && m_timerID == nIDEvent)
+    {
+        Invalidate();
+    }
+    else
+    {
+        CButton::OnTimer(nIDEvent);
+    }
+}
+
+
+void CFlashingButton::DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct)
+{
+    // This code only works with buttons.
+    ASSERT(lpDrawItemStruct->CtlType == ODT_BUTTON);
+
+    CDC context;
+    context.Attach(lpDrawItemStruct->hDC);
+
+    CRect rect;
+    rect = lpDrawItemStruct->rcItem;
+    int smudge = 3;
+    CRect focus = CRect(
+        rect.left + smudge,
+        rect.top + smudge,
+        rect.right - smudge,
+        rect.bottom - smudge
+    );
+
+    // Draw the button frame.
+    UINT style = DFCS_BUTTONPUSH;
+    if (lpDrawItemStruct->itemState & ODS_SELECTED)
+    {
+        style |= DFCS_PUSHED;
+    }
+    context.DrawFrameControl(rect, DFC_BUTTON, style);
+
+    // Draw button text
+    CString caption;
+    GetWindowText(caption);
+    context.DrawText(caption, rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+    // Draw focus
+    if (lpDrawItemStruct->itemState & ODS_FOCUS)
+    {
+        context.DrawFocusRect(focus);
+    }
+
+    // Invert for flash effect
+    if (m_shouldIFlash && (GetTickCount() / 1000) & 1)
+    {
+        context.InvertRect(focus);
+    }
+
+    context.Detach();
+}
+
+
+void CFlashingButton::StartFlashing()
+{
+    m_shouldIFlash = true;
+    if (!m_timerID)
+    {
+        m_timerID = (UINT_PTR)GetSafeHwnd();
+        UINT_PTR newTimerID = SetTimer(m_timerID, m_elapse, nullptr);
+        if (!newTimerID)
+        {
+            // TODO throw exception?
+        }
+        else
+        {
+            m_timerID = newTimerID;
+        }
+    }
+}
+
+
+void CFlashingButton::StopFlashing()
+{
+    m_shouldIFlash = false;
+    if (m_timerID)
+    {
+        KillTimer(m_timerID);
+    }
+    m_timerID = 0;
+}
--- CFlashingButton/CFlashingButton.h
+++ CFlashingButton/CFlashingButton.h
@@ -5,14 +5,22 @@
 
 class AFX_EXT_CLASS CFlashingButton : public CButton
 {
-	DECLARE_DYNAMIC(CFlashingButton)
+    DECLARE_DYNAMIC(CFlashingButton)
 
 public:
-	CFlashingButton();
-	virtual ~CFlashingButton();
+    CFlashingButton();
+    virtual ~CFlashingButton();
 
 protected:
-	DECLARE_MESSAGE_MAP()
+    bool m_shouldIFlash;
+    UINT_PTR m_timerID;
+    UINT m_elapse;
+    DECLARE_MESSAGE_MAP()
+public:
+    afx_msg void OnTimer(UINT_PTR nIDEvent);
+    virtual void DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct);
+    void StartFlashing();
+    void StopFlashing();
 };
 
 
