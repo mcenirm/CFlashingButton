229d6baca4702ba6919d3fc0cbd2e83d719fa722
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-05-07 12:23:40 -0500

    Add example app project

---
+++ Example/ExampleDlg.h
@@ -0,0 +1,33 @@
+
+// ExampleDlg.h : header file
+//
+
+#pragma once
+
+
+// CExampleDlg dialog
+class CExampleDlg : public CDialogEx
+{
+// Construction
+public:
+	CExampleDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EXAMPLE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
