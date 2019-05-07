581e180a95fcf2e151b1ee30534719ec36c6e730
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-05-07 12:44:57 -0500

    Add CFlashingButton class

---
+++ CFlashingButton/CFlashingButton.cpp
@@ -0,0 +1,29 @@
+// CFlashingButton.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "CFlashingButton.h"
+
+
+// CFlashingButton
+
+IMPLEMENT_DYNAMIC(CFlashingButton, CButton)
+
+CFlashingButton::CFlashingButton()
+{
+
+}
+
+CFlashingButton::~CFlashingButton()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CFlashingButton, CButton)
+END_MESSAGE_MAP()
+
+
+
+// CFlashingButton message handlers
+
+
