68bf68d9590d8b28f16ab6da57583552d2bc9308
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-05-08 14:08:06 -0500

    Add license boilerplate

--- CFlashingButton/CFlashingButton.h
+++ CFlashingButton/CFlashingButton.h
@@ -1,3 +1,24 @@
+/*
+
+CFlashingButton - MFC custom button that flashes
+Copyright (C) 2019  Michael Francis McEniry
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+
 #pragma once
 
 
